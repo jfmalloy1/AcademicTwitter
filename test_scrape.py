32132eb820ed2b32d9c0fd14cf7b5fa259cb12f8
John Malloy <jfmalloy1@gmail.com>
2020-05-28 15:50:27 -0700

    Speed test and sentiment analysis. Sentiment taken from Bing Liu repository

--- test_scrape.py
+++ test_scrape.py
@@ -1,23 +1,26 @@
 import GetOldTweets3 as got
 import re
+import time
 
 def main():
     #Criteria for tweets
-    max_tweets = 100
-    tweetCriteria = got.manager.TweetCriteria().setQuerySearch('leaving academia').setSince("2020-05-01").setMaxTweets(max_tweets)
+    max_tweets = [10, 100, 1000, 10000, 100000, 1000000]
+
+    #tweetCriteria = got.manager.TweetCriteria().setQuerySearch("'leaving' & 'academia'").setSince("2020-05-01").setMaxTweets(max_tweets)
 
     #Actual tweet scraping
-    tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+    for size in max_tweets:
+        tweetCriteria = got.manager.TweetCriteria().setQuerySearch("academia").setSince("2016-05-01").setMaxTweets(size)
 
-    count = 0
-    for tweet in tweets:
-        #Stupid removal of TV show called "my hero"
-        if "my hero" not in tweet.text:
-            count += 1
-            print(tweet.text)
+        start = time.time()
+        tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+        end = time.time()
 
-    #How many tweets didn't include "my hero"
-    print("Actual academic tweets:", count)
+        #TODO: clean up tweets (grammar, capitalization, "my hero academia", etc...)
+
+        #How many tweets didn't include "my hero"
+        print("Size:", len(tweets))
+        print("Time for", size, "tweets:", end-start)
 
 if __name__ == "__main__":
     main()
