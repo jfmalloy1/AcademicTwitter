1131d4a2caf31413d88ef4b666e5ecc84d876db8
John Malloy <jfmalloy1@gmail.com>
2020-07-04 15:16:03 -0700

    Added basic sentiment analysis

---
+++ neg_pos_ratio.py
@@ -0,0 +1,47 @@
+""" Goal: find the negative to positive word ratio for each quarter within dataset"""
+import pandas as pd
+
+def get_sentiment(df, year, start_month, end_month, neg, pos):
+    #Isolate date-specific tweets
+    sub_df = df[df["year"] == year]
+    sub_df = sub_df[sub_df["month"] <= end_month]
+    sub_df = sub_df[sub_df["month"] >= start_month]
+
+    #Count number of positive and negative words
+    neg_count = 0
+    pos_count = 0
+    for index, row in sub_df.iterrows():
+        txt = str(row["text"]).split(' ')
+        for word in txt:
+            if word in neg:
+                neg_count += 1
+            if word in pos:
+                pos_count += 1
+
+    print(neg_count)
+    print(pos_count)
+
+    print("Ratio for months", start_month, "-", end_month, "in", year,
+        "is:", neg_count / pos_count)
+
+def main():
+    #Read in negative and positive words
+    neg_file = open("negative_words.txt", "r")
+    neg = neg_file.read().split("\n")
+    neg_file.close()
+
+    pos_file = open("positive_words.txt", "r")
+    pos = pos_file.read().split("\n")
+    pos_file.close()
+
+    #TEST: find ratio of negative and positive words within 1st quarter of data
+    df = pd.read_csv("tweets.csv")
+    for year in [2016, 2017, 2018, 2019, 2020]:
+        get_sentiment(df, year, 1, 3, neg, pos)
+        get_sentiment(df, year, 4, 6, neg, pos)
+        if year != 2020:
+            get_sentiment(df, year, 7, 9, neg, pos)
+            get_sentiment(df, year, 10, 12, neg, pos)
+
+if __name__ == "__main__":
+    main()
